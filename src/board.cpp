6c5aef5e04d42964bb7561b26300156874f1bd02
Emile <sater.e@northeastern.edu>
2026-08-07 17:41:00 -0400

    Added comments and fixed style and guidelines

--- src/board.cpp
+++ src/board.cpp
@@ -2,8 +2,8 @@
 // 1-satbermacmar-3a
 // board.cpp
 // authors: Jacob Berman, Ethan Markow, Donald Mackinnon, Emile Sater
-// This file contains the implementation of the board class,
-// which represents a Sudoku board.
+// This file contains the implementation of the board class, which represents a
+// Sudoku board.
 
 // Declarations and functions for project #3
 #include "board.h"
@@ -42,7 +42,7 @@ void board::initialize(ifstream &fin)
 
    clear();
    
-   for (int i = 1; i <= BoardSize; i++)
+   for (int i = 1; i <= BoardSize; i++){
       for (int j = 1; j <= BoardSize; j++)
 	   {
 	       fin >> ch;
@@ -60,6 +60,7 @@ void board::initialize(ifstream &fin)
             throw fileError("Invalid number/character in sudoku board");
          }
       }
+   }
 }
 
 int squareNumber(int i, int j)
@@ -81,7 +82,6 @@ ostream &operator<<(ostream &ostr, vector<int> &v)
    }
 
    ostr << endl;
-   // I added this to prevent a warning about no return value. It was not in the original code
    return ostr; 
 }
 
@@ -90,16 +90,22 @@ ValueType board::getCell(int i, int j)
 // if bad values are passed.
 {
    if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize)
+   {
       return value[i][j];
+   }
    else
+   {
       throw rangeError("bad value in getCell");
+   }
 }
 
 bool board::isBlank(int i, int j)
 // Returns true if cell i,j is blank, and false otherwise.
 {
    if (i < 1 || i > BoardSize || j < 1 || j > BoardSize)
+   {
       throw rangeError("bad value in setCell");
+   }
 
    return (getCell(i,j) == Blank);
 }
@@ -178,6 +184,8 @@ void board::printConflicts()
 // cell is changed
 void board::updateConflicts(int row, int col, int change)
 {
+
+   // stores the value of the cell being added or removed.
    ValueType val = value[row][col];
    
    if (val == Blank)
@@ -185,8 +193,11 @@ void board::updateConflicts(int row, int col, int change)
       return;
    }
 
+   // adjusts sudoku index (1 to 9) to proper vector index (0 to 8)
    int vecIndex = val - 1;
 
+   // adjusts conflict count for specific 'vecIndex' digit in every other cell
+   // within same row
    for (int j = 1; j <= BoardSize; j++)
    {
       
@@ -196,6 +207,8 @@ void board::updateConflicts(int row, int col, int change)
       }
    }
 
+   // adjusts conflict count for specific 'vecIndex' digit in every other cell
+   // within same column
    for (int i = 1; i <= BoardSize; i++)
    {
       if (i != row)
@@ -204,10 +217,15 @@ void board::updateConflicts(int row, int col, int change)
       }
    }
 
+   // To understand what digits belong to each square, the upper left row and
+   // upper left column of EACH square (firstRow and firstCol) are found.
    int firstRow = ((row-1) / SquareSize) * SquareSize + 1;
 
    int firstCol = ((col - 1) / SquareSize) * SquareSize + 1;
 
+   // now understanding firstRow location and firstCol location of the sudoku
+   // squares, conflicts can be adjusted for each of the squares on the sudoku
+   // board.
    for (int i = firstRow; i < firstRow + SquareSize; i++)
    {
       for (int j = firstCol; j < firstCol + SquareSize; j++)
@@ -226,10 +244,13 @@ void board::updateConflicts(int row, int col, int change)
 bool board::isSolved()
 {
    
-   for (int i = 1; i <= BoardSize; i++) {
-      for (int j = 1; j <= BoardSize; j++) {
+   for (int i = 1; i <= BoardSize; i++)
+   {
+      for (int j = 1; j <= BoardSize; j++)
+      {
          int cellValue = getCell(i,j);
-         if (isBlank(i,j) || conflicts[i][j][cellValue-1] > 0) {
+         if (isBlank(i,j) || conflicts[i][j][cellValue-1] > 0)
+         {
             cout << "The board is not solved." << endl;
             return false;
          }
--- src/board.h
+++ src/board.h
@@ -24,9 +24,8 @@ typedef int ValueType; // The type of the value in a cell
 const int Blank = -1;  // Indicates that a cell is blank
  
 const int SquareSize = 3;  //  The number of cells in a small square
-                           //  (usually 3).  The board has
-                           //  SquareSize^2 rows and SquareSize^2
-                           //  columns.
+                           //  (usually 3).  The board has SquareSize^2 rows
+                           //  and SquareSize^2 columns.
 
 const int BoardSize = SquareSize * SquareSize;
 
@@ -67,8 +66,7 @@ class board{
         void resetCell(int, int); 
 
     private:
-        // updates the conflicts of the cells that are effected 
-        // by a change
+        // updates the conflicts of the cells that are effected by a change
         void updateConflicts(int row, int col, int change);
 
         // data members for the board class
